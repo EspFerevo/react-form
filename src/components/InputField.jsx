1fd4107017d468f20ab53a4cd112fcc4005d264b
unknown <olekchekrenov@gmail.com>
2025-02-21 12:48:17 +0200

    Update InputField.jsx

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,1 +1,24 @@
 // Поле введення
+
+import React from "react";
+
+const InputField = ({ label, type, name, register, errors, placeholder }) => {
+  return (
+    <div className="mb-4">
+      <label className="block text-gray-700 font-semibold">{label}</label>
+      <input
+        type={type}
+        {...register(name)}
+        placeholder={placeholder}
+        className={`w-full p-2 border ${
+          errors[name] ? "border-red-500" : "border-gray-300"
+        } rounded-md`}
+      />
+      {errors[name] && (
+        <p className="text-red-500 text-sm">{errors[name].message}</p>
+      )}
+    </div>
+  );
+};
+
+export default InputField;
